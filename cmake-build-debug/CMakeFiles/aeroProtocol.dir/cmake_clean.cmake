33e78e42e1e88cc1c408c2f72fa01db422595450
Scott Lee <scott.lee@nucleusscientific.com>
2017-02-06 00:14:05 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/aeroProtocol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aeroProtocol.dir/main.c.o"
+  "aeroProtocol.pdb"
+  "aeroProtocol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aeroProtocol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
